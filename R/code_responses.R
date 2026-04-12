592d7b35f4332576053783f30aa59888c1e1fd08
franikowsp <philipp.franikowski@gmail.com>
2026-04-12 19:16:40 +0200

    add geometry_variables option

--- R/code_responses.R
+++ R/code_responses.R
@@ -3,11 +3,10 @@
 #' @param coding_scheme List or character. Coding scheme of a unit from IQB Studio Lite.
 #' @param responses List or character. Unit responses from IQB Testcenter.
 #' @param manual List or character. Manual codes to potentially overwrite response data. Defaults to `"null"` (would be treated in the same way as `NA`).
-#' @param ggb List or character. GeoGebra variables to potentially refill response data. Defaults to `"null"` (would be treated in the same way as `NA`).
 #'
 #' @return A data frame.
 #' @export
-code_responses <- function(coding_scheme, responses, manual = "null", ggb = "null") {
+code_responses <- function(coding_scheme, responses, manual = "null") {
   eatAutoCode$call("codeResponses",
                    list(codingScheme = coding_scheme,
                         responses = responses,
